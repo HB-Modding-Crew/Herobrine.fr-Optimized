6d488976ca104191e6d061264a582c7327d8e750
Paul Brancieq <paul.bancieq@epitech.eu>
2023-01-21 20:02:47 +0100

    Reset

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.11
-
-# Copie le fichier requirements.txt dans le répertoire de travail
-COPY requirements.txt .
-
-# Installe les dépendances à partir du fichier requirements.txt
-RUN pip install -r requirements.txt
--- src/common/OutputWrapper.py
+++
@@ -1,8 +0,0 @@
-from textwrap import TextWrapper
-from src.const import Indents
-
-
-class OutputWrapper(TextWrapper):
-    def __init__(self, indent_size: Indents):
-        self.__indent = indent_size.value * "    "
-        super().__init__(initial_indent=self.__indent, subsequent_indent=self.__indent)
